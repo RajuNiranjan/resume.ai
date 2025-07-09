9fafa460b1790b59684e84273afbcfb0388967f7
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-07-09 08:42:46 +0530

    added cors in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.resume_router import resume_router
 
-app=FastAPI()
+app = FastAPI()
 
-@app.get('/')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  
+    allow_credentials=True,
+    allow_methods=["*"],                    
+    allow_headers=["*"],                      
+)
+
+@app.get("/")
 def index():
-    return {"message":"Welcome to Resume.AI"}
+    return {"message": "Welcome to Resume.AI"}
 
-
-app.include_router(resume_router, prefix='/api')
+app.include_router(resume_router, prefix="/api")
