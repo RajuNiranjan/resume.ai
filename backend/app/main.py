c3351529cee213043edb2be17f430576c7f1b9ea
RajuNiranjan <rajuniranjan1910@gmail.com>
2025-09-28 08:30:11 +0530

    added cors in backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.core.config import get_settings
 from app.core.database import connect_to_mongo, close_mongo_server
 from app.routes.v1.auth import auth_router
-
 
 settings = get_settings()
 
@@ -22,6 +22,18 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+origins = [
+    "http://localhost:3000",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,         
+    allow_credentials=True,
+    allow_methods=["*"],           
+    allow_headers=["*"],           
+)
+
 @app.get("/")
 async def root():
     return {"message": f"Welcome to {settings.PROJECT_NAME}", "version": settings.VERSION}
@@ -30,5 +42,4 @@ async def root():
 async def health_check():
     return {"message": "Yup! I'm Good"}
 
-
-app.include_router(auth_router, prefix='/v1/api/auth', tags=["Authentication"])
+app.include_router(auth_router, prefix="/v1/api/auth", tags=["Authentication"])
